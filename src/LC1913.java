814cbee7653068849c225651d2f43592128704a8
BondarEgorTest <96645276+Egor1515@users.noreply.github.com>
2023-05-22 14:29:26 +1000

    index.html

--- src/LC1913.java
+++ src/LC1913.java
@@ -27,5 +27,14 @@ public class LC1913 {
             nums[n - i - 1] = temp;
         }
         return (nums[n-1]*nums[n-2])-(nums[0]*nums[1]);
+    }   public static int maxDiffere2nc1e(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            int temp = nums[i];
+            nums[i] = nums[n - i - 1];
+            nums[n - i - 1] = temp;
+        }
+        return (nums[n-1]*nums[n-2])-(nums[0]*nums[1]);
     }
 }
